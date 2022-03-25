cd38b9da26dff3d136415140483af4cab1213dc3
Ashutosh Maurya <ashumaurya486@gmail.com>
2022-03-25 07:59:15 +0530

    Portfolio starting

---
+++ src/Component/NavBar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <ul className="d-flex">
+      <li>HOME</li>
+      <li>ABOUT</li>
+      <li>CONTACT</li>
+      <li>MORE</li>
+    </ul>
+  );
+};
+
+export default NavBar;
